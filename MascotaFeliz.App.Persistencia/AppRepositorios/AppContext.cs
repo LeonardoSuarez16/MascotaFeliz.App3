5132a46e872c91807bbc7e9977879625c8ae395f
Leonardo Suarez Restrepo <leonardosuarez16@hotmail.com>
2022-10-03 11:31:39 -0500

    proyecto final mascotafeliz con conecion lista para despliege en paquetico de mani

--- MascotaFeliz.App.Persistencia/AppRepositorios/AppContext.cs
+++ MascotaFeliz.App.Persistencia/AppRepositorios/AppContext.cs
@@ -1,23 +1,32 @@
-using Microsoft.EntityFrameworkCore;
 using MascotaFeliz.App.Dominio;
+using Microsoft.EntityFrameworkCore;
 
 namespace MascotaFeliz.App.Persistencia
 {
     public class AppContext : DbContext
     {
-        public DbSet<Persona> Personas {get;set;}
-        public DbSet<Veterinario> Veterinarios {get;set;}
-        public DbSet<Dueno> Duenos {get;set;}
-        public DbSet<VisitaPyP> VisitasPyP {get;set;}
-        public DbSet<Historia> Historias {get;set;}
-        public DbSet<Mascota> Mascotas {get;set;}
+        public DbSet<Persona> Personas { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Veterinario> Veterinarios { get; set; }
+
+        public DbSet<Dueno> Duenos { get; set; }
+
+        public DbSet<VisitaPyP> VisitasPyP { get; set; }
+
+        public DbSet<Historia> Historias { get; set; }
+
+        public DbSet<Mascota> Mascotas { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder
+        )
         {
             if (!optionsBuilder.IsConfigured)
             {
+                // optionsBuilder
+                //.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MascotaFelizData");
                 optionsBuilder
-                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MascotaFelizData");
+                    .UseSqlServer("Data Source = leosuarez.mssql.somee.com; Initial Catalog = leosuarez;user id=leosuarez16_SQLLogin_1;pwd=vj2mklnzqx");
             }
         }
     }
